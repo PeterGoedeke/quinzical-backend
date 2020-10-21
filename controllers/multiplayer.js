fb9dd11b490df4a20e4013b7ef643fec0dc05a65
Peter Goedeke <peterbgoedeke@gmail.com>
2020-10-21 16:56:48 +1300

    Refactored lobby to use socket IO

--- controllers/multiplayer.js
+++ controllers/multiplayer.js
@@ -1,135 +1,107 @@
-const lobbies = {}
-
-const random = (min, max) => Math.floor(Math.random() * (max - min) ) + min
-
-function getNewLobbyCode() {
-    const code = random(1000, 10000)
-    return lobbies[code] ? getNewLobbyCode : code
-}
-
-function getMember(lobby, _id) {
-    for (const member of lobby.members) {
-        if (member._id == _id) {
-            return member
+module.exports = function(io) {
+    const lobbies = {}
+    
+    const random = (min, max) => Math.floor(Math.random() * (max - min) ) + min
+    
+    function getNewLobbyCode() {
+        const code = random(1000, 10000)
+        return lobbies[code] ? getNewLobbyCode : code
+    }
+    
+    function getMember(lobby, _id) {
+        for (const member of lobby.members) {
+            if (member._id == _id) {
+                return member
+            }
+        }
+        return null
+    }
+    
+    function messageLobby(id, type, content) {
+        for(const socket of io.sockets.sockets) {
+            if (!getMember(lobbies[id], socket.jwt._id)) {
+                continue
+            }
+            socket.emit(type, JSON.stringify(content))
         }
     }
-    return null
-}
-
-function messageLobby(id, type, content) {
-    for(const client of wsInstance.getWss().clients) {
-        if (!getMember(lobbies[id], client.jwt._id)) {
-            continue
-        }
-        client.send(JSON.stringify({
-            type,
-            ...content
-        }))
-    }
-}
-function messageMember(id, type, content) {
-
-}
-
-function isCorrect(answer, lobby) {
-    if (answer == lobby.currentQuestion) {
-        return true
-    }
-    return false
-}
-
-function onMessage(message, ws) {
-    console.log(message)
-    const code = message.code
-    if (message.type == 'ANSWER') {
-        const lobby = lobbies[code]
-        const result = isCorrect(message.answer, lobby)
-
-        const scoreIncrease = result ? 100 : 0
-        const member = getMember(lobby, ws.jwt._id)
-        member.score += scoreIncrease
-
-        ws.send(JSON.stringify({
-            type: 'RESULT',
-            answerIsCorrect: result,
-            score: member.score
-        }))
-    }
-    else if (message.type == 'CREATE_LOBBY') {
-        console.log('ayy')
-        const questions = message.questions
-        console.log(ws.jwt)
-        const code = getNewLobbyCode()
-        const lobby = {
-            host: ws.jwt.username,
-            questions,
-            code,
-            members: [{
-                _id: ws.jwt._id,
-                score: 0,
-                avatar: message.avatar
-            }]
-        }
-        lobbies[code] = lobby
-        ws.send(JSON.stringify({
-            type: 'LOBBY_ID',
-            content: lobby.code
-        }))
-    }
-    else if (message.type == 'NEXT_QUESTION') {
-        const lobby = lobbies[code]
-        if (lobby.host == ws.jwt.username) {
-            lobby.currentQuestion = lobby.questions.pop()
-
-            if (lobby.currentQuestion) {
-                ws.send(JSON.stringify({
-                    type: 'NEXT_QUESTION',
-                    content: lobby.currentQuestion
-                }))
+    
+    /**
+     * 
+     * @param {SocketIO.Socket} socket 
+     */
+    function handle(socket) {
+        socket.on('RESULT', data => {
+            const code = data.code
+            const lobby = lobbies[code]
+    
+            const member = getMember(lobby, socket.jwt._id)
+            member.score = data.score
+            lobby.membersAnswered ++
+            if (lobby.membersAnswered == lobby.members.length) {
+                socket.emit('ROUND_OVER', JSON.stringify(lobby.members))
+            }
+        })
+        socket.on('CREATE_LOBBY', data => {
+            const questions = data.questions
+            const code = getNewLobbyCode()
+            const lobby = {
+                host: socket.jwt.username,
+                questions,
+                code,
+                members: [{
+                    _id: socket.jwt._id,
+                    score: 0,
+                    avatar: data.avatar
+                }]
+            }
+            lobbies[code] = lobby
+            console.log(lobby.code)
+            socket.emit('LOBBY_ID', JSON.stringify({ code: lobby.code }))
+        })
+        socket.on('NEXT_QUESTION', data => {
+            const code = data.code
+            const lobby = lobbies[code]
+            if (lobby.host == socket.jwt.username) {
+                lobby.currentQuestion = lobby.questions.pop()
+                lobby.membersAnswered = 0
+    
+                if (lobby.currentQuestion) {
+                    socket.emit('NEXT_QUESTION', JSON.stringify(lobby.currentQuestion))
+                }
+                else {
+                    socket.emit('GAME_OVER', JSON.stringify(lobby))
+                }
             }
             else {
-                ws.send(JSON.stringify({
-                    type: 'GAME_OVER',
-                    content: lobby
-                }))
+                console.log('nope')
             }
-        }
-        else {
-            console.log('nope')
-        }
-        // lobby.currentInterval = setInterval(() => {
-            
-        // }, 30000)
+        })
+        socket.on('JOIN_LOBBY', data => {
+            const code = data.code
+            const lobby = lobbies[code]
+            if (!lobby) {
+                socket.emit('INVALID_LOBBY', JSON.stringify('Invalid lobby'))
+                return
+            }
+            if (!getMember(lobby, socket.jwt._id)) {
+                lobby.members.push({
+                    _id: socket.jwt._id,
+                    score: 0,
+                    avatar: data.avatar
+                })
+                socket.emit('LOBBY_JOINED', JSON.stringify(lobby))
+            }
+        })
+        socket.on('LEAVE_LOBBY', data => {
+            const code = data.code
+            const lobby = lobbies[code]
+            lobby.members = lobby.members.splice(lobby.members.indexOf(getMember(lobby, socket.jwt._id)))
+            messageLobby(code, 'LEFT', socket.jwt.username)
+        })
     }
-    else if (message.type == 'JOIN_LOBBY') {
-        const lobby = lobbies[code]
-        if (!lobby) {
-            ws.send(JSON.stringify({
-                type: "ERROR",
-                content: "Invalid lobby"
-            }))
-            return
-        }
-        if (!getMember(lobby, ws.jwt._id)) {
-            lobby.members.push({
-                _id: ws.jwt._id,
-                score: 0,
-                avatar: message.avatar
-            })
-            ws.send(JSON.stringify({
-                lobby
-            }))
-        }
+    
+    return {
+        handle
     }
-    else if (message.type == 'LEAVE_LOBBY') {
-        const lobby = lobbies[code]
-        lobby.members = lobby.members.splice(lobby.members.indexOf(getMember(lobby, ws.jwt._id)))
-        messageLobby(code, 'LEFT', ws.jwt.username)
-    }
-
-    console.log(lobbies)
-}
-
-module.exports = {
-    onMessage
 }
