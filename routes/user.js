3d4db08a204bfcd8bbf697632e829fdb95bd9932
Peter Goedeke <peterbgoedeke@gmail.com>
2020-10-24 15:35:19 +1300

    Added leaderboard route

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const authCtrl = require('../controllers/auth')
 const userCtrl = require('../controllers/user')
 
 router.put('/updatescore', authCtrl.authorise, userCtrl.updateScore)
+router.get('/leaderboard', authCtrl.authorise, userCtrl.getLeaderboard)
 
 module.exports = router
